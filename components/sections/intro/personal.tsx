940e1d6ab62d04f70b756eb42e809a04e539ccab
quang-pham-dev <quangpn.developer@gmail.com>
2025-06-28 09:10:33 +0700

    update: disable blog link

--- components/sections/intro/personal.tsx
+++ components/sections/intro/personal.tsx
@@ -1,14 +1,38 @@
-import { ArrowUpRightIcon } from "lucide-react"
 import Link from "next/link"
 
 import { AnimatedFade } from "@/components/animated-fade"
-import { Card, CardContent } from "@/components/ui/card"
 import { useTranslations } from "@/hooks/use-translation"
+import type { TranslationKey } from "@/i18n/type"
+import AnimatedCards from "./animated-card"
 
 const projectsUrl = "https://projects.quangpham.dev"
 const blogUrl = "https://blog.quangpham.dev"
 const resumeUrl =
 	"https://drive.google.com/file/d/1iENqZzAUHFqjaBsGjQ1Tj9c5UxlQFtPx/view"
+
+const PERSONAL_INFO = [
+	{
+		id: 1,
+		url: resumeUrl,
+		text: "myResume",
+	},
+	{
+		id: 2,
+		url: projectsUrl,
+		text: "myProjects",
+	},
+	{
+		id: 3,
+		url: blogUrl,
+		text: "myBlog",
+		isDisabled: true,
+	},
+]
+
+const currentCompanies = {
+	name: "Glo Digital",
+	link: "https://glo.com",
+}
 
 export default function Personal() {
 	const { t } = useTranslations()
@@ -23,52 +47,24 @@ export default function Personal() {
 						className="underline"
 						target="_blank"
 						rel="noopener noreferrer"
-						href="https://restfulmind.co/"
+						href={currentCompanies.link}
 					>
-						Restful Mind
+						{currentCompanies.name}
 					</Link>
 				</span>
 
 				<div className="grid gap-2 sm:grid-cols-3">
-					<Card className="group relative overflow-hidden transition-colors hover:bg-accent">
-						<CardContent className="px-1 py-2 flex items-center justify-center h-full">
-							<a
-								className="flex items-center underline font-semibold"
-								href={resumeUrl}
-								target="_blank"
-								rel="noopener noreferrer"
-							>
-								{t("myResume")}
-								<ArrowUpRightIcon className="h-5 w-5 transition-transform group-hover:translate-x-1 group-hover:-translate-y-1" />
-							</a>
-						</CardContent>
-					</Card>
-					<Card className="group relative overflow-hidden transition-colors hover:bg-accent">
-						<CardContent className="px-1 py-2 flex items-center justify-center h-full">
-							<a
-								className="flex items-center underline font-semibold"
-								href={projectsUrl}
-								target="_blank"
-								rel="noopener noreferrer"
-							>
-								{t("myProjects")}
-								<ArrowUpRightIcon className="h-5 w-5 transition-transform group-hover:translate-x-1 group-hover:-translate-y-1" />
-							</a>
-						</CardContent>
-					</Card>
-					<Card className="group relative overflow-hidden transition-colors hover:bg-accent">
-						<CardContent className="px-1 py-2 flex items-center justify-center h-full">
-							<a
-								className="flex items-center underline font-semibold"
-								href={blogUrl}
-								target="_blank"
-								rel="noopener noreferrer"
-							>
-								{t("myBlogs")}
-								<ArrowUpRightIcon className="h-5 w-5 transition-transform group-hover:translate-x-1 group-hover:-translate-y-1" />
-							</a>
-						</CardContent>
-					</Card>
+					{PERSONAL_INFO.map((item) => {
+						const { url, text, isDisabled } = item
+						return (
+							<AnimatedCards
+								key={text}
+								url={url}
+								text={text as TranslationKey}
+								isDisabled={isDisabled}
+							/>
+						)
+					})}
 				</div>
 			</div>
 		</AnimatedFade>
